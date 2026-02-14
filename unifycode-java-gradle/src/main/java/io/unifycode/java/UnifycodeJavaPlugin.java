2630d175314b5894e5f4828deb8d0902d252f3cb
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2026-02-14 18:22:39 +0300

    Initial folder structure

---
+++ unifycode-java-gradle/src/main/java/io/unifycode/java/UnifycodeJavaPlugin.java
@@ -0,0 +1,10 @@
+package io.unifycode.java;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class UnifycodeJavaPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+    }
+}
